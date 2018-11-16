1fb8619ea3865608b860fcf1f47cff4d2cbac01d
Arahant <arahant.ashok@gmail.com>
2018-11-16 16:20:24 +0530

    Update Nature.java

--- java/com/sk/program/Nature.java
+++ java/com/sk/program/Nature.java
@@ -3,14 +3,15 @@ package com.sk.program;
 public class Nature {
 
     /*
+     * The aspects of human personality captured in 4 dimensions
     private enum dimension1 {MORALITY, IMMORALITY} (Red)
     private enum dimension2 {INTEGRITY, FRAGILITY} (Green)
-    private enum dimension3 {COXISTENCE, SURVIVALISM} (Blue)
-    The Base Natures are Benevolence (Blue), Indifference (Green) and Malevolence (Red)
+    private enum dimension3 {COEXISTENCE, SURVIVALISM} (Blue)
+    private enum dimension4 {RATIONAL, IRRATIONAL}
     */
 
-	public Nature(float d1, float d2, float d3) {
-        this.nature = d1+"-"+d2+"-"+d3;
+	public Nature(float d1, float d2, float d3, float d4) {
+        this.nature = d1+"-"+d2+"-"+d3+"-"+d4;
     }
 	
 	private String nature;
@@ -27,37 +28,37 @@ public class Nature {
 
     public String getNatureType() {
     	String[] dimensions = this.nature.split("-");
-    	float RH = Float.parseFloat(dimensions[0]);
-		float GH = Float.parseFloat(dimensions[1]);
-		float BH = Float.parseFloat(dimensions[2]);
-		int d1 = RH>0.5?1:0; 
-		int d2 = GH>0.5?1:0;
-		int d3 = BH>0.5?1:0;
-		String n = d1+"."+d2+"."+d3;
+    	float AH = Float.parseFloat(dimensions[0]);
+		float BH = Float.parseFloat(dimensions[1]);
+		float CH = Float.parseFloat(dimensions[2]);
+		float DH = Float.parseFloat(dimensions[3]);
+		int d1 = AH>0.5?1:0; 
+		int d2 = BH>0.5?1:0;
+		int d3 = CH>0.5?1:0;
+		int d4 = DH>0.5?1:0;
+		String n = d1+"."+d2+"."+d3+"."+d4;
 		String type="";
 		switch(n) {
-			case "1.1.1": 
+			case "1.1.1.1":
+			case "1.1.1.0":
+			case "1.0.1.1": 
+			case "1.0.1.0":
 				type = BENEVOLENT;
 				break;
-			case "1.1.0": 
+			case "1.1.0.1": 
+			case "1.1.0.0":
+			case "1.0.0.1": 
+			case "0.1.1.1": 
+			case "0.0.1.1": 
+			case "0.0.1.0":
 				type = INDIFFERENT;
 				break;
-			case "1.0.1": 
-				type = BENEVOLENT;
-				break;
-			case "1.0.0": 
-				type = INDIFFERENT;
-				break;
-			case "0.1.1": 
-				type = INDIFFERENT;
-				break;
-			case "0.1.0": 
-				type = MALEVOLENT;
-				break;
-			case "0.0.1": 
-				type = INDIFFERENT;
-				break;
-			case "0.0.0": 
+			case "1.0.0.0":
+			case "0.1.1.0":
+			case "0.1.0.1":
+			case "0.1.0.0":
+			case "0.0.0.1":
+			case "0.0.0.0":
 				type = MALEVOLENT;
 				break;
 		}
