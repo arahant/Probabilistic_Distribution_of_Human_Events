1e10227b019eb02194dd1fc3c1af024cb8aa6fa1
Arahant <arahant.ashok@gmail.com>
2018-11-20 17:45:28 +0530

    Add files via upload

---
+++ java/com/sk/program/action/Event.java
@@ -0,0 +1,31 @@
+package com.sk.program.action;
+
+import java.sql.Date;
+
+import com.sk.program.nature.Nature;
+
+public class Event {
+	
+	private String name;
+	private Nature nature;
+	private Date date;
+	
+	public void describeEvent(String nm, Date dt, Nature nat) {
+		this.name = nm;
+		this.date = dt;
+		this.nature = nat;  
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public Nature getNature() {
+		return nature;
+	}
+
+}
