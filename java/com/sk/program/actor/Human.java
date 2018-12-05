5be79668f7bc1d240b628de2805706a21f64a5d9
Arahant <arahant.ashok@gmail.com>
2018-12-05 11:05:08 +0530

    Update Human.java

--- java/com/sk/program/actor/Human.java
+++ java/com/sk/program/actor/Human.java
@@ -1,9 +1,11 @@
 package com.sk.program.actor;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.sk.program.action.Action;
+import com.sk.program.action.Event;
 import com.sk.program.nature.Nature;
 
 public class Human {
@@ -13,10 +15,16 @@ public class Human {
 	private String name;
 	
 	private float anxietyThreshold;
+	private float proximity;
 	private Nature personality;
 	private LinkedHashMap<Human,Float> connections;
+	private static List<Human> population;
 	
 	private static int c = 0;
+	
+	static {
+		population = new LinkedList<>();
+	}
 	
 	public Human(float d1, float d2, float d3, float d4) {
 		id = ++c;
@@ -41,6 +49,22 @@ public class Human {
 		return name;
 	}
 	
+	// connections
+	public void addConnection(Human arg, float weight) {
+		connections.put(arg,weight);
+	}
+	public LinkedHashMap<Human,Float> getConnection() {
+		return connections;
+	}
+	
+	public static void updatePopulation(Human arg) {
+		population.add(arg);
+	}
+	public static List<Human> getPopulation() {
+		return population;
+	}
+	
+	// group
 	public void setGroup(Group group) {
 		this.group = group;
 		influencePersonality();
@@ -90,27 +114,29 @@ public class Human {
 		return personality;
 	}
 	
-	public void addConnection(Human arg, float weight) {
-		connections.put(arg,weight);
-	}
-	public LinkedHashMap<Human,Float> getConnection() {
-		return connections;
-	}
-	
+	// anxiety threshold
 	public void resetAnxietyThreshold(float t) {
 		this.anxietyThreshold = t;
 	}
 	public float getAnxietyThreshold() {
 		return anxietyThreshold;
 	}
+	
+	// proximity
+	public void setProximity(float pr) {
+		this.proximity = pr;
+	}
+	public float getProximity() {
+		return proximity;
+	}
 
 	// An action influences both the actor and target(s) and changes the probability of the nature of the action
-	public void commitAction(Human actor, List<Human> targets, List<Action> actions) {
+	public void commitAction(List<Human> targets, List<Action> actions) {
 		for(int i=0;i<targets.size();i++) {
 			Human target = targets.get(i);
 			Action action = actions.get(i);
 			target.influencePersonality(action,false);
-			actor.influencePersonality(action,true);
+			this.influencePersonality(action,true);
 			action.updateProbability();
 		}
 	}
@@ -136,7 +162,59 @@ public class Human {
 		
 		// updating the influence by the probability of the type of the human's personality
 		String type = this.personality.getNatureType();
-		float pr = (float)action.getProbability(type);
+		float pr = (float)Action.getProbability(type);
+		
+		// the influence is less pronounced when the human nature is INDIFFERENT
+		float inf = 1f;
+		if(type.equals(Nature.INDIFFERENT))
+			inf = 0.01f;
+		
+		// effective change in nature
+		dA*=pr*act*inf;
+		dB*=pr*act*inf;
+		dC*=pr*act*inf;
+		dD*=pr*act*inf;
+		
+		// normalizing the altered nature's dimensions
+		AH = (AH+dA)/(1+dA); 
+		BH = (BH+dB)/(1+dB); 
+		CH = (CH+dC)/(1+dC);
+		DH = (DH+dD)/(1+dD);
+		Nature newNature = new Nature(AH,BH,CH,DH);
+		this.setPersonality(newNature);
+	}
+	
+	// reaction to any event by the actor 
+	public void eventReaction(Event event) {
+		for(Human link:this.getConnection().keySet()) {
+			link.influencePersonality(event,false);
+			this.influencePersonality(event,true);
+			Action.updateProbability(event);
+		}
+	}
+	
+	private void influencePersonality(Event event, boolean actor) {
+		String[] human_nature = this.personality.getNature().split("-");
+		float AH = Float.parseFloat(human_nature[0]);
+		float BH = Float.parseFloat(human_nature[1]);
+		float CH = Float.parseFloat(human_nature[2]);
+		float DH = Float.parseFloat(human_nature[3]);
+
+		// the change in the nature influenced by the action
+		String[] event_nature = event.getNature().getNature().split("-");
+		float dA = Float.parseFloat(event_nature[0]);
+		float dB = Float.parseFloat(event_nature[1]);
+		float dC = Float.parseFloat(event_nature[2]);
+		float dD = Float.parseFloat(event_nature[3]);
+
+		// the influence is less pronounced on the actor than on the recipient
+		float act = 1f;
+		if(actor)
+			act = 0.01f;
+		
+		// updating the influence by the probability of the type of the human's personality
+		String type = this.personality.getNatureType();
+		float pr = (float)Action.getProbability(type);
 		
 		// the influence is less pronounced when the human nature is INDIFFERENT
 		float inf = 1f;
